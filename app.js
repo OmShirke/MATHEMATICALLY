cdb14eeb4b454c200aafb0e21cf9454d71d805f0
OmShirke <113794365+OmShirke@users.noreply.github.com>
2023-03-25 02:22:22 +0530

    index was updated

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs')
-
+app.listen(4000)
 // middleware and static files
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true}))
 
-
+app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
     res.render('index', { title : 'MATHEMATICALLY'})
 })
